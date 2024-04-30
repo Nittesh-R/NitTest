f0ae56ae8d72fa8763d46a5143fb6bc27d7fb95a
Nittesh.R <164860190+Nittesh-R@users.noreply.github.com>
2024-04-30 11:56:42 +0530

    Update automation.py
    added missed webdriver ,webdriver.commom.by, webdriver.support.ui , imports

--- automation.py
+++ automation.py
@@ -3,7 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
- 
+
+
  
 # Initialize the Chrome WebDriver
 driver = webdriver.Chrome()
